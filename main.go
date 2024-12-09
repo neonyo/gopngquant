9b2ae8e7fce2af4826b2e91257dd6c4eebbba4f1
yangjiaming <jiaming.yang@tophant.com>
2024-12-09 17:03:51 +0800

    上传图片

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ func main() {
 		}
 		defer RemoveFile(inputFile, 30*time.Second)
 		outputFile := GetLocalFile(ext)
+
 		//fmt.Println(fmt.Sprintf("pngquant --quality=65-80 %s --output %s", inputFile, outputFile))
 		cmd := exec.Command("pngquant", "--quality=40-80", inputFile, "--output", outputFile, "--speed=10")
 		var o bytes.Buffer
